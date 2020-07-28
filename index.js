7d35676c7d9021a5582dabbbf89961c23253e3a4
Josh Guha <joshguha@gmail.com>
2020-07-28 21:56:22 +0100

    env variables altered

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
 
 // set up mongoose
 mongoose.connect(
-    process.env.MONGODB_CONNECTION_STRING,
+    process.env.MONGODB_URI,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
